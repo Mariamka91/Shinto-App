cd736e77ae9ad8829e1b10d11069f9c034452726
Byron Tottaram <byron.tottaram@alumni.stonybrook.edu>
2020-04-11 16:18:27 -0400

    pushing new canvas file with updated drag and drop feature

--- shinto_react/src/components/P5Canvas/index.js
+++ shinto_react/src/components/P5Canvas/index.js
@@ -1,115 +1,82 @@
-import React, { Component } from 'react';
-import p5 from 'p5';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import React, { Component } from "react";
+import p5 from "p5";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class P5Canvas extends Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
 
-    constructor(props) {
+  sketch = p => {
+    p.newImage = null;
+    p.x = 0;
+    p.y = 0;
 
-        super(props);
-        this.canvasRef = React.createRef();
-        this.state = {x: 0, y: 0}
-    }
+    p.setup = () => {
+      this.can = p.createCanvas(600, 600);
+      p.background(0);
+      this.clear = p.createButton("clear");
+      this.clear.mousePressed(() => p.background(0));
+      this.can.drop(p.gotFile);
+    };
 
+    p.draw = () => {
+      if (p.mouseIsPressed) {
+        p.stroke(255);
+        p.strokeWeight(10);
+        p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+      }
 
-    sketch = (p) => {
+      if (p.newImage) {
+        p.image(
+          p.newImage,
+          this.x - p.newImage.width * 0.2,
+          this.y - p.newImage.height * 0.2,
+          p.newImage.width * 0.5,
+          p.newImage.height * 0.5
+        );
+      }
+    };
 
-        p.newImage = null
+    p.gotFile = file => {
+      console.log(file);
 
-        p.setup = () => {
-            this.can = p.createCanvas(500, 500);
-            p.background(0)
-            this.clear = p.createButton("clear");
-            this.clear.mousePressed(() => p.background(0))
-            this.can.drop(p.gotFile)
-            this.save = p.createButton("save");
-        }
+      this.x = p.mouseX;
+      this.y = p.mouseY;
 
+      switch (file.type) {
+        case "audio":
+          console.log("this is an audio file");
+          break;
+        case "image":
+          p.newImage = p.createImg(file.data, "").hide();
+          break;
 
-        p.draw = () => {
+        default:
+          console.log("I dont know what this file is?");
+          break;
+      }
+    };
 
-            if (p.mouseIsPressed) {
-                p.stroke(255);
-                p.strokeWeight(10)
-                p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
-            }
+    // we attach our own function to our P5 instance 'p'
+    p.myClear = () => {
+      p.background(155);
+    };
+  };
 
-            if (p.newImage){
+  componentDidMount() {
+    this.myP5 = new p5(this.sketch, this.canvasRef.current);
+    this.testX = 5555;
+  }
 
-                p.image(
-                    p.newImage,
-                    this.state.x - this.canvasRef.current.getBoundingClientRect().left,
-                    this.state.y - this.canvasRef.current.getBoundingClientRect().top,
-                    p.width * 0.2,
-                    p.height * 0.2);
-            }
-        }
-
-
-        p.gotFile = (file) => {
-            console.log(file);
-
-            switch (file.type) {
-                case "audio":
-                    console.log("this is an audio file")
-                    break;
-                case "image":
-                   p.newImage = p.createImg(file.data,"").hide();
-                    break;
-
-                default:
-                    console.log("I dont know what this file is?")
-                    break;
-            }
-
-
-        }
-
-
-        // we attach our own function to our P5 instance 'p'
-        p.myClear = () => { p.background(155) }
-    }
-
-    componentDidMount() {
-        this.myP5 = new p5(this.sketch, this.canvasRef.current)
-        this.testX = 5555;
-        this.canvasRef.current.addEventListener("mouseleave", evt =>
-        this.setState({ x: evt.x , y: evt.y })
-      );
-        
-    }
-
-    // We can call our own function in our p5 code through our instance P5 'myP5'
-    // If we want to call any of our own p5 functions from outside our 'sketch' function,
-    // (as for examply calling p.myClear from our button below), we have call it from
-    // another class function.
-    // We do this because if we try to attach to our button onClick={this.myP5.myClear}
-    // This will not work and we will get an error because 'this.myP5.myClear' only exists
-    // after 'componentDidMount' and does not exist at the time of first render when it is bound
-    // to the buttons onClick.
-
-
-        render() {
-            if (this.canvasRef.current) {
-              console.log(
-                this.state.x - this.canvasRef.current.getBoundingClientRect().left,
-                this.state.y - this.canvasRef.current.getBoundingClientRect().top
-              );
-              console.log(this.state.x, this.state.y);
-
-            }
-
-        return (
-            <>
-                <div ref={this.canvasRef} style={{ margin: "50px", width: "500px", height: "500px" }}>
-                </div>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div ref={this.canvasRef} style={{ marginTop: "50px" }}></div>
+      </>
+    );
+  }
 }
 
-
-export default P5Canvas
-
+export default P5Canvas;
