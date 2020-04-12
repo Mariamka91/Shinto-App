46add8874aa28782c092ef0c9c80e3bd0454ca1c
Mariam Kilasonia <54387462+Mariamka91@users.noreply.github.com>
2020-04-12 14:36:36 -0400

    Navbar (#6)
    * add Navbar and save button
    
    * add Navbar

--- shinto_react/src/App.js
+++ shinto_react/src/App.js
@@ -1,35 +1,13 @@
 import React from 'react';
-import Canvas from './components/P5Canvas';
-import * as ReactBootstrap from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import P5Canvas from './components/P5Canvas/index';
 
 function App() {
   return (
-    <div className="App">
-    <ReactBootstrap.Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
-  <ReactBootstrap.Navbar.Brand href="#home">Kami no Michi</ReactBootstrap.Navbar.Brand>
-  <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootstrap.Nav className="mr-auto">
-      <ReactBootstrap.Nav.Link href="#features"></ReactBootstrap.Nav.Link>
-      <ReactBootstrap.Nav.Link href="#pricing"></ReactBootstrap.Nav.Link>
-      <ReactBootstrap.NavDropdown title="options" id="collasible-nav-dropdown">
-
-        <ReactBootstrap.NavDropdown.Item href="#action/3.1">logged in as </ReactBootstrap.NavDropdown.Item>
-        <ReactBootstrap.NavDropdown.Item href="#action/3.2">log out</ReactBootstrap.NavDropdown.Item>
-        <ReactBootstrap.NavDropdown.Item href="#action/3.3">Create Kamvas</ReactBootstrap.NavDropdown.Item>
-        <ReactBootstrap.NavDropdown.Divider />
-        <ReactBootstrap.NavDropdown.Item href="#action/3.4">Search</ReactBootstrap.NavDropdown.Item>
-      </ReactBootstrap.NavDropdown>
-    </ReactBootstrap.Nav>
-    <ReactBootstrap.Nav>
-      <ReactBootstrap.Nav.Link href="#deets"></ReactBootstrap.Nav.Link>
-      <ReactBootstrap.Nav.Link eventKey={2} href="#memes">
-      </ReactBootstrap.Nav.Link>
-    </ReactBootstrap.Nav>
-  </ReactBootstrap.Navbar.Collapse>
-</ReactBootstrap.Navbar>
-      <Canvas />
-      
+    <div>
+  <Navbar/>
+  <P5Canvas/>
     </div>
   );
 }
